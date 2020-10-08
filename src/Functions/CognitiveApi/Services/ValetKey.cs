2f23141741b95cdbd868badf8dae04127f08f7fd
Hugo Girard <hugirard@microsoft.com>
2020-10-08 16:02:16 -0400

    added valet key azure function

---
+++ src/Functions/CognitiveApi/Services/ValetKey.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CognitiveApi
+{
+    public class ValetKey
+    {
+
+        public string GetSharedAccessReferenceView(string blobName,
+                                                    string accountName,
+                                                    string containerName)
+        {           
+            var storageSharedKeyCredential = new StorageSharedKeyCredential(accountName, Environment.GetEnvironmentVariable("AudioFilesStorageKey"));
+
+            var blobSasBuilder = new BlobSasBuilder
+
+            {
+                BlobContainerName = containerName,
+                BlobName = blobName,
+                Resource = "b",
+                StartsOn = DateTimeOffset.UtcNow.AddMinutes(-5),
+                ExpiresOn = DateTimeOffset.UtcNow.AddDays(1)
+            };
+            blobSasBuilder.SetPermissions(BlobSasPermissions.Read);
+            return blobSasBuilder.ToSasQueryParameters(storageSharedKeyCredential).ToString();            
+        }
+    }
+}
