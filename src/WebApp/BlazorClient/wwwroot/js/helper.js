026cd8fa36d6d2d10a48d30d74509b9efaa49cfd
Hugo Girard <hugirard@microsoft.com>
2020-10-08 17:30:24 -0400

    added logic for client

---
+++ src/WebApp/BlazorClient/wwwroot/js/helper.js
@@ -0,0 +1,8 @@
+﻿function downloadFromUrl(url) {
+
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = '';
+    anchorElement.click();
+    anchorElement.remove();
+}
