69510118537acc22e98ec9ae690771ea411956f8
Hugo Girard <hugirard@microsoft.com>
2020-10-08 11:41:54 -0400

    added client

---
+++ src/WebApp/BlazorClient/Model/Message.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Model
+{
+    public class Message
+    {
+        [Required(ErrorMessage = "Please enter a message")]
+        [StringLength(100, ErrorMessage = "The message is too long")]
+        [JsonProperty("TextToConvert")]
+        public string Text { get; set; }
+    }
+}
