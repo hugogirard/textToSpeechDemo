f4f5db97d96759d56b835f9e30bb2dbd4431ac21
Hugo Girard <hugirard@microsoft.com>
2020-10-13 12:01:50 -0400

    modify azure function

--- src/Functions/CognitiveApi/CognitiveApi.cs
+++ src/Functions/CognitiveApi/CognitiveApi.cs
@@ -13,6 +13,9 @@ using CognitiveApi.Model;
 using Microsoft.CognitiveServices.Speech.Audio;
 using Microsoft.Azure.Storage.Queue.Protocol;
 using System.Runtime.CompilerServices;
+using Azure.Storage.Files.Shares;
+using System.Xml;
+using Microsoft.Net.Http.Headers;
 
 namespace CognitiveApi
 {
@@ -67,45 +70,50 @@ namespace CognitiveApi
             }
 
             string blobUri;
+            
             try
-            {
-                MemoryStream ms = null;                
+            {                
+                string filename = $"{Guid.NewGuid()}.wav";
+
                 using (var synthesizer = new SpeechSynthesizer(SpeechConfig, null))
                 {
                     log.LogInformation("SpeechSynthesizer created");
 
-                    var result = await synthesizer.SpeakTextAsync(speechInfo.TextToConvert);
+                    // Write a temp file
+                    string txt = WriteXmlFile(speechInfo.TextToConvert);
 
-                    log.LogInformation("Result for text to audio");
-                    log.LogInformation($"Result reason {result.Reason}");
-
-                    if (result.Reason == ResultReason.Canceled)
+                    var result = await synthesizer.SpeakSsmlAsync(txt);
+                    using (var stream = AudioDataStream.FromResult(result))
                     {
-                        log.LogInformation($"Get cancellation result {result.Reason}");
-                        var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+                        log.LogInformation("Result for text to audio");
+                        log.LogInformation($"Result reason {result.Reason}");
 
-                        // Something went wrong
-                        if (cancellation.Reason == CancellationReason.Error)
+                        if (result.Reason == ResultReason.Canceled)
                         {
-                            log.LogError($"ErrorCode={cancellation.ErrorCode} - ErrorDetails={cancellation.ErrorDetails}");
-                            return CreateErrorResponse();
+                            log.LogInformation($"Get cancellation result {result.Reason}");
+                            var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+
+                            // Something went wrong
+                            if (cancellation.Reason == CancellationReason.Error)
+                            {
+                                log.LogError($"ErrorCode={cancellation.ErrorCode} - ErrorDetails={cancellation.ErrorDetails}");
+                                return CreateErrorResponse();
+                            }
                         }
-                    }
 
-                    log.LogInformation("Getting memory object");
-
-                    ms = new MemoryStream(result.AudioData);                    
+                        log.LogInformation("Saving file");
+                                             
+                        await stream.SaveToWaveFileAsync(filename);
+                    }                                    
                 }
-                
-                string filename = $"{Guid.NewGuid()}.wav";
-                
+                                                
                 log.LogInformation("Getting blob reference");
 
                 var blob = container.GetBlockBlobReference(filename);
 
                 log.LogInformation("Uploading to blob reference");
 
-                await blob.UploadFromStreamAsync(ms);
+                await blob.UploadFromFileAsync(filename);
 
                 // Get SAS
                 string sas = ValetKey.GetSharedAccessReferenceView(filename, 
@@ -116,13 +124,13 @@ namespace CognitiveApi
 
                 blobUri = $"{blob.Uri}?{sas}";
 
+                File.Delete(filename);
+
                 log.LogInformation("Blob uploaded");
             }
             catch (Exception ex)
             {
                 throw ex;
-                //log.LogError("Cannot process text to audio file", ex);
-                //return CreateErrorResponse();
             }
 
             return new OkObjectResult(blobUri);            
@@ -133,6 +141,20 @@ namespace CognitiveApi
             var result = new ObjectResult("Internal Server Error");
             result.StatusCode = StatusCodes.Status500InternalServerError;
             return result;
+        }
+
+        private static string WriteXmlFile(string text) 
+        {
+                        
+            string audioTxt = @$"<speak version=""1.0"" xmlns=""http://www.w3.org/2001/10/synthesis"" xmlns:mstts=""https://www.w3.org/2001/mstts"" xml:lang=""en-US"">
+                                  <voice name=""en-US-JennyNeural"">
+                                    <mstts:express-as style=""chat"">
+                                      {text}
+                                    </mstts:express-as>
+                                  </voice>
+                                </speak>";
+
+            return audioTxt;
         }
     }
 }
